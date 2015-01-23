987a3d9b0d0f508e1896a71f62b7aa49f5d9409e
Stijn <Stijn.heylen@groept.be>
2015-01-23 13:53:53 +0100

    Initial commit

---
+++ 01ServletJspIntro/src/main/java/servlet/TestApp.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//Zorg dat onze pojo als servlet herkend wordt.
+@WebServlet("/Welkom")
+public class TestApp extends HttpServlet {
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Toevoegen van de boodschap aan de request
+		request.setAttribute("message", "Hallo " + request.getParameter("naam")
+				+ "<br>Welkom bij Ontwerpen met Java<br>mvg<br>je docent.");
+		// We sturen de request naar onze resultaat pagina
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/results/DisplayMSG.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
---
+++ 01ServletJspIntro/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<form action="Welkom">
+	<input type=text name=naam />
+	<input type=submit value="test">
+</form>
+</body>
+</html>
